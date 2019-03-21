6b6ba09bf0f1b72838f8a233a6e46df877144f53
Joe Ganobsik <jganobsik@gmail.com>
2019-03-21 01:45:24 +0000

    added CLI intro with input validation

--- lib/stockindex.rb
+++ lib/stockindex.rb
@@ -1,7 +1,5 @@
 require "stockindex/version"
 require "stockindex/cli"
-require "nokogiri"
-require "pry"
 require "stockindex/scraper"
 require "stockindex/indexes"
 
--- lib/stockindex/cli.rb
+++ lib/stockindex/cli.rb
@@ -1,6 +1,21 @@
 class Stockindex::CLI
   
   def start 
-    puts 'hello!'
+    puts 'Welcome to StockIndex!'
+    puts "Which index would you like to see?"
+    puts "1) S&P 2) NASDAQ 3)DOW JONES"
+    puts "Please enter a number 1-3"
+    input = gets.strip.to_i
+    
+    case input
+    when 1 
+      puts "S&P"
+    when 2 
+      puts "NASDAQ"
+    when 3 
+      puts "DOW JONES"
+    else 
+      puts "Invalid Selection"
+    end
   end
 end
