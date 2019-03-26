6a2711785079011a8e29926032e9e5b86fa956aa
Joe Ganobsik <jganobsik@gmail.com>
2019-03-26 00:48:04 +0000

    asks user whether they wish to select another index after first selection

--- lib/stockindex/cli.rb
+++ lib/stockindex/cli.rb
@@ -8,7 +8,7 @@ class Stockindex::CLI
     def index
     puts "Which index would you like to see?"
     puts "1) S&P 2) NASDAQ 3)RUSSELL 4) NASDAQ GLOBAL 5)DOW JONES "
-    puts "Please enter a number 1-3"
+    puts "Please enter a number 1-5"
     input = gets.strip.to_i
     
     case input
@@ -17,28 +17,31 @@ class Stockindex::CLI
      ticker = "SPX"
      url = "https://www.nasdaq.com/aspx/infoquotes.aspx?symbol=SPX&selected=SPX"
      scrape_index(url, ticker)
+     exit_prompt
     when 2 
       puts "NASDAQ"
       ticker = "IXIC"
       url = "https://www.nasdaq.com/aspx/infoquotes.aspx?symbol=IXIC&selected=IXIC"
       scrape_index(url, ticker)
+      exit_prompt
     when 3 
       puts "RUSSELL 2000"
       ticker = "RUT"
       url = "https://www.nasdaq.com/aspx/infoquotes.aspx?symbol=RUT&selected=RUT"
       scrape_index(url, ticker)
-      
+      exit_prompt
     when 4
       puts "NASDAQ GLOBAL"
       ticker = "NQGS"
       url = "https://www.nasdaq.com/aspx/infoquotes.aspx?symbol=NQGS&selected=NQGS"
       scrape_index(url, ticker)
-      
+      exit_prompt
     when 5   
       puts "DOW JONES"
       ticker = "INDU"
       url = "https://www.nasdaq.com/aspx/infoquotes.aspx?symbol=INDU&selected=INDU"
       scrape_index(url, ticker)
+      exit_prompt
     else 
       puts "Invalid Selection"
       index
@@ -58,4 +61,19 @@ class Stockindex::CLI
     end
  end
   
+  def exit_prompt
+    puts "Would you like to view another index?"
+    puts "type yes or no"
+    input = gets.strip.downcase
+    
+    if input == "yes"
+      index
+    elsif input == "no"
+      puts "Thanks for using StockIndex!"
+    else 
+     puts "Invalid Selection"
+      exit_prompt
+    end
+  end
+      
 end
